2c9e4582b5eb9b140cdd0ad5b7b1dfefe0b8e011
ContardiC <c.contardi@spacecoding.it>
2025-04-02 10:32:07 +0200

    add: solutions ex5, ex6

--- Closures/main.swift
+++ Closures/main.swift
@@ -36,3 +36,26 @@ let celsiusToFahrenheit : (Double) -> Double = {
 print(celsiusToFahrenheit(0))
 
 // EX 5:
+let sumArray : ([Int]) -> Int = { arr in 
+    var tot = 0
+    for n in arr{
+        tot += n
+    }
+    return tot
+}
+
+print(sumArray([3,4,5,1,2]))
+
+// EX 6
+let filterOddNumbers : ([Int]) -> [Int] = {
+    arr in 
+    var oddNumbers = [Int]()
+    for n in arr {
+        if n % 2 != 0{
+            oddNumbers.append(n)
+        }
+    }
+    return oddNumbers
+}
+
+print(filterOddNumbers([2,3,4,5,6,7]))
